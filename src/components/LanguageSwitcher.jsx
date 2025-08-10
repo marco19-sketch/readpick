42d13342af47c77baedbb8b460675f43ecad521d
marco19-sketch <marco.brusca1970@gmail.com>
2025-08-10 09:19:05 +0200

    Set Italian as  default

--- src/components/LanguageSwitcher.jsx
+++ src/components/LanguageSwitcher.jsx
@@ -1,6 +1,5 @@
 import { useTranslation } from "react-i18next";
-// import gbFlag from "../assets/images/32x24/gb.webp";
-// import itFlag from "../assets/images/32x24/it.webp";
+
 
 export default function LanguageSwitcher() {
   const { t, i18n } = useTranslation();
@@ -20,19 +19,21 @@ export default function LanguageSwitcher() {
           src={`/flags/20x-width/gb.webp`}
           srcSet={`/flags/20x-width/gb.webp 1x, /flags/40x-width/gb.webp 2x`}
           alt={t("englishFlag")}
+          fetchPriority="high"
+          loading="eager"
         />
       </button>
       <button
         className={`italiano ${i18n.language === "it" ? "selected" : ""}`}
         onClick={() => changeLanguage("it")}>
-        {/* <img src={itFlag} alt={t("italianFlag")} /> */}
         <img
-          // loading="lazy"
           width="20"
           height="16"
           src={`/flags/20x-width/it.webp`}
           srcSet={`/flags/20x-width/it.webp 1x, /flags/40x-width/it.webp 2x`}
           alt={t("italianFlag")}
+          fetchPriority="high"
+          loading="eager"
         />
       </button>
     </div>
--- src/i18n.js
+++ src/i18n.js
@@ -1,15 +1,21 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import HttpBackend from "i18next-http-backend";
+import LanguageDetector from "i18next-browser-languagedetector";
 
 i18n
-  .use(HttpBackend) // abilita fetch via HTTP
+  .use(HttpBackend) // fetch translations via HTTP
+  .use(LanguageDetector) // detect and remember last selected language
   .use(initReactI18next)
   .init({
-    lng: "en", // o 'it'
-    fallbackLng: "en",
+    fallbackLng: "it", // fallback if language file is missing
     backend: {
-      loadPath: "/locales/{{lng}}.json", // fetch da /public/locales/it.json, ecc
+      loadPath: "/locales/{{lng}}.json", // e.g. /public/locales/it.json
+    },
+    detection: {
+      // Options for LanguageDetector
+      order: ["localStorage", "cookie", "navigator"],
+      caches: ["localStorage"], // remember language in localStorage
     },
     interpolation: {
       escapeValue: false,
