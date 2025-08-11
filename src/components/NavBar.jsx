394b73d25768292be635fa131df045a85051dda8
marco19-sketch <marco.brusca1970@gmail.com>
2025-08-11 14:43:27 +0200

    Implemented hidden Login button on NavBar when in Login page

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,4 +1,4 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 import "./NavBar.css";
 import { useLogout } from "../hooks/useLogout";
 import { FaHome } from "react-icons/fa";
@@ -10,6 +10,9 @@ import useIsMobile from "../hooks/useIsMobile";
 export default function NavBar({ t, favorites, login, setLogin }) {
   const logout = useLogout();
   const isMobile = useIsMobile();
+  const location = useLocation();
+
+  const isLoginPage = location.pathname === "/login";
 
   return (
     <nav>
@@ -33,11 +36,13 @@ export default function NavBar({ t, favorites, login, setLogin }) {
             logout();
             setLogin(false);
           }}>
-         {isMobile ? <IoLogOut /> : (t('logout') || 'Esci')}
+          {isMobile ? <IoLogOut /> : t("logout") || "Esci"}
         </button>
+      ) : isLoginPage ? (
+        ""
       ) : (
         <NavLink className="login" to="/login">
-          {isMobile ? <IoLogIn /> : (t("login") || "Accedi")}
+          {isMobile ? <IoLogIn /> : t("login") || "Accedi"}
         </NavLink>
       )}
     </nav>
